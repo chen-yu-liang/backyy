bfa649f57b41bf221755847ba4bd7e83cff22813
chen-yu-liang <33013102+chen-yu-liang@users.noreply.github.com>
2018-03-31 16:15:58 +0800

    fix...

--- backyy/src/main/java/com/lv/controller/AgentController.java
+++ backyy/src/main/java/com/lv/controller/AgentController.java
@@ -10,13 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * 
 * @className : AgentController.java
-* @Description : 代理商表
-* @author ： Administrator  
-* @date : 2018年3月31日
+* @Description : 浠ｇ悊鍟嗚〃
+* @author 锛� Administrator  
+* @date : 2018骞�3鏈�31鏃�
  */
 @Scope("prototype")
 @Controller
 @RequestMapping("/Agent")
 public class AgentController extends BaseController {
+	private String name = "asdf";
 
 }
